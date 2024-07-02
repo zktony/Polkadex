c4817680df4dba8403426b07363a5845eb5708a5
gautham <gauthamastro@gmail.com>
2024-07-02 10:13:04 +0530

    Remove old code

--- runtimes/mainnet/build.rs
+++ runtimes/mainnet/build.rs
@@ -20,18 +20,13 @@
 #[docify::export(template_enable_metadata_hash)]
 fn main() {
 	substrate_wasm_builder::WasmBuilder::init_with_defaults()
-		.enable_metadata_hash("UNIT", 12)
+		.enable_metadata_hash("PDEX", 12)
 		.build();
 }
 
 #[cfg(all(feature = "std", not(feature = "metadata-hash")))]
 fn main() {
 	substrate_wasm_builder::WasmBuilder::build_using_defaults();
-	// WasmBuilder::new()
-	// 	.with_current_project()
-	// 	.import_memory()
-	// 	.export_heap_base()
-	// 	.build()
 }
 
 /// The wasm builder is deactivated when compiling
